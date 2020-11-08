476bc9c614e37c8b04c9bbb1d2c49d87dab501b2
ze3000A <37904859+ze3000A@users.noreply.github.com>
2020-11-08 12:18:57 +0800

    Update new_read_speed.py

--- new_read_speed.py
+++ new_read_speed.py
@@ -1,62 +1,79 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on 2020.11.06    09:33
+Created on 2020.11.08    11:29
 @author: XHR
-超声测距函数，调用distance函数，返回值为一个浮点值，单位cm
+霍尔脉冲读取函数
 """
-#霍尔脉冲读取函数
 
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)  #使用bcm引脚编号方式
-GPIO.setwarnings(False)  #禁用警告。如果RPi.GRIO检测到一个引脚已经被设置成了非默认值，会有警告信息。
+def my_lu_callback(lu_channel):
+    global lu_counter  #设置为全局变量
+    if GPIO.event_detected(lu_pin):        #检测到一个脉冲则脉冲数加1
+        lu_counter++ 
 
-lu_pin = 19  #编码器连接引脚定义
-ld_pin = 26
-ru_pin = 16
-rd_pin = 20
+def my_ld_callback(ld_channel):
+    global ld_counter  #设置为全局变量
+    if GPIO.event_detected(ld_pin):        #检测到一个脉冲则脉冲数加1
+        ld_counter++ 
 
-GPIO.setup(lu_pin, GPIO.IN,pull_up_down=GPIO.PUD_UP)   #gpio口设置为上拉，输入模式
-GPIO.setup(ld_pin, GPIO.IN,pull_up_down=GPIO.PUD_UP)
-GPIO.setup(ru_pin, GPIO.IN,pull_up_down=GPIO.PUD_UP)  
-GPIO.setup(rd_pin, GPIO.IN,pull_up_down=GPIO.PUD_UP)   
+def my_ru_callback(ru_channel):
+    global ru_counter  #设置为全局变量
+    if GPIO.event_detected(ru_pin):        #检测到一个脉冲则脉冲数加1
+        ru_counter++ 
 
+def my_rd_callback(rd_channel):
+    global rd_counter  #设置为全局变量
+    if GPIO.event_detected(rd_pin):        #检测到一个脉冲则脉冲数加1
+        rd_counter++ 
 
-'''
-chan_list = [19,26,16,20]  #bcm编号的引脚，对应board引脚的35，37，36，38
-GPIO.setup(chan_list, GPIO.IN,pull_up_down=GPIO.PUD_UP)
-'''
+def readspeed():
+    '''
+        GPIO.setwarnings(False)  #禁用警告。如果RPi.GRIO检测到一个引脚已经被设置成了非默认值，会有警告信息。
+        GPIO.setup(io_pin, GPIO.IN,pull_up_down=GPIO.PUD_UP)   #gpio口设置为上拉，输入模式
+        io_counter=0      #左前轮脉冲初值
+    '''
 
-lu_counter=0      #左前轮脉冲初值
-ld_counter=0     #左后轮脉冲初值
-ru_counter=0      #右前轮脉冲初值
-rd_counter=0     #右后轮脉冲初值
- 
-def my_lu_callback(lu_channel):          #边缘检测回调函数，详情在参见链接中
- global lu_counter  #设置为全局变量
- if GPIO.event_detected(lu_pin):        #检测到一个脉冲则脉冲数加1
-  lu_counter++         
-def my_ld_callback(ld_channel):          #边缘检测回调函数，详情在参见链接中
- global ld_counter                 #设置为全局变量
- if GPIO.event_detected(ld_pin):        #检测到一个脉冲则脉冲数加1
-  ld_counter++
-def my_ru_callback(ru_channel):          #边缘检测回调函数，详情在参见链接中
- global ru_counter                 #设置为全局变量
- if GPIO.event_detected(ru_pin):        #检测到一个脉冲则脉冲数加1
-  ru_counter++
-def my_rd_callback(rd_channel):          #边缘检测回调函数，详情在参见链接中
- global rd_counter                 #设置为全局变量
- if GPIO.event_detected(rd_pin):        #检测到一个脉冲则脉冲数加1
-  rd_counter++
+    GPIO.setmode(GPIO.BCM)  #使用bcm引脚编号方式
+    GPIO.setwarnings(False)  #禁用警告。如果RPi.GRIO检测到一个引脚已经被设置成了非默认值，会有警告信息。
 
-GPIO.add_event_detect(lu_pin,GPIO.RISING,callback=my_lu_callback) #在引脚上添加上升临界值检测再回调
-GPIO.add_event_detect(ld_pin,GPIO.RISING,callback=my_ld_callback) #在引脚上添加上升临界值检测再回调
-GPIO.add_event_detect(ru_pin,GPIO.RISING,callback=my_ru_callback) #在引脚上添加上升临界值检测再回调
-GPIO.add_event_detect(rd_pin,GPIO.RISING,callback=my_rd_callback) #在引脚上添加上升临界值检测再回调
+    lu_pin = 19  #编码器连接引脚定义
+    ld_pin = 26
+    ru_pin = 16
+    rd_pin = 20
+    
+    GPIO.setup(lu_pin, GPIO.IN,pull_up_down=GPIO.PUD_UP)   #gpio口设置为上拉，输入模式
+    GPIO.setup(ld_pin, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(ru_pin, GPIO.IN,pull_up_down=GPIO.PUD_UP)  
+    GPIO.setup(rd_pin, GPIO.IN,pull_up_down=GPIO.PUD_UP)   
+    
+    '''
+    chan_list = [19,26,16,20]  #bcm编号的引脚，对应board引脚的35，37，36，38
+    GPIO.setup(chan_list, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+    '''
+    
+    lu_counter=0      #左前轮脉冲初值
+    ld_counter=0     #左后轮脉冲初值
+    ru_counter=0      #右前轮脉冲初值
+    rd_counter=0     #右后轮脉冲初值
+    
+    GPIO.add_event_detect(lu_pin,GPIO.RISING,callback=my_lu_callback) #在引脚上添加上升临界值检测再回调
+    GPIO.add_event_detect(ld_pin,GPIO.RISING,callback=my_ld_callback) #在引脚上添加上升临界值检测再回调
+    GPIO.add_event_detect(ru_pin,GPIO.RISING,callback=my_ru_callback) #在引脚上添加上升临界值检测再回调
+    GPIO.add_event_detect(rd_pin,GPIO.RISING,callback=my_rd_callback) #在引脚上添加上升临界值检测再回调
+    
+    GPIO.cleanup([lu_pin,ld_pin,ru_pin,rd_pin])   #释放gpio口
+    return 0
 
-
-GPIO.cleanup()   #释放gpio口
-
-def read_io_speed(io_pin):
- 
+if __name__ == "__main__":
+    try:
+        while True:
+            speed = readspeed()
+            print("Measured Distance = {:.2f} cm".format(dist))
+            time.sleep(1)
+	
+    # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
