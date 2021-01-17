f214fc079b94d9cdeb3eb5bd5049b7298828712c
ze3000A <37904859+ze3000A@users.noreply.github.com>
2021-01-17 21:59:12 +0800

    Update new_read_speed.py

--- new_read_speed.py
+++ new_read_speed.py
@@ -7,11 +7,7 @@ Created on 2020.11.08    11:29
 """
 
 import RPi.GPIO as GPIO
-
-global lu_counter=0  #设置为全局变量
-global ld_counter=0  #设置为全局变
-global ru_counter=0  #设置为全局变量
-global rd_counter=0  #设置为全局变量
+import time
 
 def my_lu_callback(lu_channel):
     if GPIO.event_detected(lu_pin):        #检测到一个脉冲则脉冲数加1
@@ -64,16 +60,16 @@ def readspeed():
     GPIO.add_event_detect(ru_pin,GPIO.RISING,callback=my_ru_callback) #在引脚上添加上升临界值检测再回调
     GPIO.add_event_detect(rd_pin,GPIO.RISING,callback=my_rd_callback) #在引脚上添加上升临界值检测再回调
     
+    time.sleep(1)
+    print(lu_counter,ld_counter,ru_counter,rd_counter)
     GPIO.cleanup([lu_pin,ld_pin,ru_pin,rd_pin])   #释放gpio口
     return 0
 
 if __name__ == "__main__":
     try:
         while True:
-            globle lu_counter=ld_counter=ru_counter=rd_counter=0
             speed = readspeed()
-            print(lu_counter，ld_counter,ru_counter,rd_counter)
-            time.sleep(1)
+            #print(lu_counter，ld_counter,ru_counter,rd_counter)
 	
     # Reset by pressing CTRL + C
     except KeyboardInterrupt:
